c165dfd2b9b36881af57050395a479f61eb6ae02
Dell_JESMQZ <jesmqz@gmail.com>
2025-07-21 22:36:27 -0400

    Creating service

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from '../task.service';
+
+@Component({
+    selector: 'app-new-task',
+    imports: [FormsModule],
+    templateUrl: './new-task.component.html',
+    styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  constructor (private tasksService: TasksService) {}
+
+  onAddTask(title: string, description: string) {
+    this.tasksService.addTask({ title, description });
+    this.formEl()?.nativeElement.reset();
+  }
+}
